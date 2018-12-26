0085f471b94cb69f504a71a1c8761b80320ddeff
Andrew "&y" Haskell <xcrnr2013@gmail.com>
2018-12-26 13:08:42 -0500

    Add source maps to the test build
    This way, if a test assertion fails, we know what line of code the
    assertion was originally on

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ let glob = require("glob");
 
 let entry = __dirname + "/app/src/page.js";
 let outputPath = __dirname + "/dist/";
+let devtool = "";
 if (process.env.TESTBUILD) {
   entry = glob.sync(__dirname + "/app/test/**/*.test.js");
   outputPath = __dirname + "/test-dist/";
+  devtool = "source-map";
 }
 
 module.exports = {
@@ -28,4 +30,5 @@ module.exports = {
       },
     ],
   },
+  devtool: devtool,
 };
