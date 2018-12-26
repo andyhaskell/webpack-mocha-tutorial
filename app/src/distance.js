13a7f43fb1a0f92445bcd67d99a5586e3c8adcb0
Andrew "&y" Haskell <xcrnr2013@gmail.com>
2018-12-26 12:02:15 -0500

    Initial web app
    Imports using a stack of <script> tags in the HTML

---
+++ app/src/distance.js
@@ -0,0 +1,16 @@
+// distance takes in two points, represented as objects with numeric x and y
+// values and returns the distance between them
+//
+// [TODO] Use great-circle distance, not 2D distance, which we're only using to
+// keep the code in this tutorial short
+function distance(p2, p1) {
+  let yDist = p2.y - p1.y;
+  let xDist = p2.x - p1.x;
+  return Math.sqrt(Math.pow(yDist, 2) + Math.pow(xDist, 2));
+}
+
+// sortByDistance takes in your location and an array of points and returns the
+// array of points sorted
+function sortByDistance(myPt, points) {
+  return points.sort((pt1, pt2) => distance(pt1, myPt) - distance(pt2, myPt));
+}
---
+++ app/src/page.js
@@ -0,0 +1,9 @@
+let stores = [
+  {name: "Cambridge Naturals", x: -71.1189, y: 42.3895},
+  {name: "Sarah's Market", x: -71.1311, y: 42.3823},
+  {name: "Whole Foods Fresh Pond", x: -71.142, y: 42.3904},
+];
+
+let here = {name: "You are here", x: -71.147, y: 42.3834};
+let nearest = sortByDistance(here, stores)[0];
+document.getElementById("nearest-store").innerHTML = nearest.name;
