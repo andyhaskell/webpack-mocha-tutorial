8064767238ea4639f46b9c01b1e5f3ed475eb54c
Andrew "&y" Haskell <xcrnr2013@gmail.com>
2018-12-26 12:24:11 -0500

    Add Babel loader to our webpack config file
    This loader converts our modern JavaScript into more
    backward-compatible JavaScript, creating a webpack bundle that works
    on older browsers.

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,20 @@ module.exports = {
   output: {
     path: __dirname + "/dist/",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: ["/node_modules/"],
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
